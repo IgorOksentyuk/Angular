910d5c09909ec8dca003f1edfcf39b5f1ea3c566
IgorOks <winbka.lem@gmail.com>
2022-12-08 16:59:46 +0200

    ls_15 | Created redirecting to products page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
+    path: 'products',
+    pathMatch: 'full',
+    component: ProductsComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
