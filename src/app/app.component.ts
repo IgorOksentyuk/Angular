0dbf38843c464b5fc3dc00b110ef6c60a6107904
IgorOks <winbka.lem@gmail.com>
2022-12-09 15:37:57 +0200

    ls_15 | Remove all module logic to products.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,8 @@
 import { Component } from '@angular/core';
 
-interface IData {
-  id: number;
-  name: string;
-  price: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public title = 'angular';
-
-  public data = this.generateData(8);
-
-  generateData(count: number): IData[] {
-    let dataArray = [];
-    let pizzaNames = [
-      'Vegan',
-      'Pepperoni',
-      'Hot',
-      'Chicken',
-      'Karri',
-      '4 cheeses',
-      'Potato',
-      'Tasty',
-      'Tasteless',
-      'Italiano',
-      'Ukrainian',
-    ];
-
-    for (let index = 0; index < count; index++) {
-      const randomName =
-        pizzaNames[Math.floor(Math.random() * pizzaNames.length)];
-
-      dataArray.push({
-        id: index,
-        name: randomName + ' pizza',
-        price: Number((Math.random() * 3).toFixed(2)),
-      });
-    }
-
-    return dataArray;
-  }
-}
+export class AppComponent {}
