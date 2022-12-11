93aad2776276e2b1593fe918b2c52cb2e5fb3373
IgorOks <winbka.lem@gmail.com>
2022-12-11 22:17:45 +0200

    ls_16 | Added details component imports and declarations

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -6,11 +6,18 @@ import { ProductsService } from '../services/products.service';
 import { ProductComponent } from './product/product.component';
 import { HeaderComponent } from './header/header.component';
 import { ChangePriceColorDirective } from './directives/change-price-color.directive';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
-  declarations: [ProductComponent, HeaderComponent, ChangePriceColorDirective],
-  imports: [CommonModule],
+  declarations: [
+    ProductComponent,
+    HeaderComponent,
+    ChangePriceColorDirective,
+    ProductDetailsComponent,
+  ],
+  imports: [CommonModule, ProductsRoutingModule],
   providers: [ProductsService],
-  exports: [ProductComponent, HeaderComponent],
+  exports: [ProductComponent, HeaderComponent, ProductDetailsComponent],
 })
 export class ProductsModule {}
