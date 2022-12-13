368f31d8b01f56c10e6019a67f4b962030fd261b
IgorOks <winbka.lem@gmail.com>
2022-12-13 11:07:11 +0200

    ls_16 | Created product-details component,that renders procucts info

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IData } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: IData | undefined;
+  productId: number;
+  products: IData[];
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private svc: ProductsService
+  ) {}
+
+  ngOnInit() {
+    this.productId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.products = this.svc.data;
+    this.product = this.products.find((el) => el.id == this.productId);
+  }
 }
