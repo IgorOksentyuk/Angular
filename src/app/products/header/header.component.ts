12fc814cec04833f45abd75a09bfe848158babfd
IgorOks <winbka.lem@gmail.com>
2022-12-14 11:05:08 +0200

    ls_16 | Added count of added in cart products

--- src/app/products/header/header.component.ts
+++ src/app/products/header/header.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private cartService: CartService) {}
+
+  getCount() {
+    return this.cartService.getCount();
+  }
+}
