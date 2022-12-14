395adf62c2f236351e1ef33c5ba91949ad747aea
IgorOks <winbka.lem@gmail.com>
2022-12-14 11:01:26 +0200

    ls_16 | Added cart route

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductsComponent } from './products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,10 @@ const routes: Routes = [
   {
     path: 'products/:id',
     component: ProductDetailsComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
 ];
 
