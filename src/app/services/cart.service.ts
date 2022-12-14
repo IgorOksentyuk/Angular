263b5dd0492039246f5b96c0a721c7a0106cb392
IgorOks <winbka.lem@gmail.com>
2022-12-14 10:54:05 +0200

    ls_16 | Created service with cart logic

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { IData } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: IData[] = [];
+
+  addToCart(product: IData) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getCount() {
+    return this.items.length;
+  }
+
+  getTotal() {
+    let total = 0;
+
+    for (let item of this.items) {
+      total += item.price;
+    }
+
+    return Number(total.toFixed(2));
+  }
+
+  removeItem(id: number) {
+    let index = this.items.findIndex((el) => el.id === id);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+    console.log(id);
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
