5d06cb2ac57ffe7e6c0cf03d34522a5af861432d
IgorOks <winbka.lem@gmail.com>
2022-12-14 11:04:01 +0200

    ls_16 | Created addToCart method

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IData } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
@@ -12,15 +13,21 @@ export class ProductDetailsComponent implements OnInit {
   product: IData | undefined;
   productId: number;
   products: IData[];
+  item: IData;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private svc: ProductsService
+    private svc: ProductsService,
+    private cartService: CartService
   ) {}
 
   ngOnInit() {
     this.productId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     this.products = this.svc.data;
     this.product = this.products.find((el) => el.id == this.productId);
+  }
+
+  addToCart(product: IData) {
+    this.cartService.addToCart(product);
   }
 }
