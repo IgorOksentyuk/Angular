4715d570df292df446c616a0be60a31ba45d841b
IgorOks <winbka.lem@gmail.com>
2022-12-14 21:54:58 +0200

    ls_16 | Added tooltip logic

--- src/app/products/header/header.component.ts
+++ src/app/products/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { IData } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private cartService: CartService) {}
+export class HeaderComponent implements OnInit {
+  @Input()
+  product: IData;
+  visibleTool: boolean;
+
+  items: IData[] = this.cartService.getItems();
+  totalPrice: number;
+
+  constructor(private cartService: CartService) {
+    this.visibleTool = false;
+  }
+
+  ngOnInit(): void {
+    this.totalPrice = this.cartService.getTotal();
+  }
 
   getCount() {
     return this.cartService.getCount();
+  }
+
+  removeItem(id: number) {
+    this.cartService.removeItem(id);
+    this.totalPrice = this.cartService.getTotal();
   }
 }
