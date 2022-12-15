04e9e3e66004dbae09ea07c1e818ba1d76cc7f26
IgorOks <winbka.lem@gmail.com>
2022-12-15 13:23:39 +0200

    ls_16 | Remove redirect to app-routing module

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,29 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProductsComponent } from './products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'products',
-  },
   {
     path: 'products',
     pathMatch: 'full',
-    loadChildren: () =>
-      import('./products.component').then((m) => m.ProductsComponent),
+    component: ProductsComponent,
   },
   {
     path: 'products/:id',
-    loadChildren: () =>
-      import('./product-details/product-details.component').then(
-        (m) => m.ProductDetailsComponent
-      ),
+    component: ProductDetailsComponent,
   },
   {
     path: 'cart',
-    loadChildren: () =>
-      import('./cart/cart.component').then((m) => m.CartComponent),
+    component: CartComponent,
   },
 ];
 
