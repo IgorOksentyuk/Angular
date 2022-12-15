d212d46ab6885de5d3cea620d2447b3e9669fa03
IgorOks <winbka.lem@gmail.com>
2022-12-15 13:27:28 +0200

    ls_15 | Reworked lazy-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    pathMatch: 'full',
+    loadChildren: () => import('./app.module').then((m) => m.AppModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
