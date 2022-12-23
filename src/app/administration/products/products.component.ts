0369384aa15d37812364fe3b51f13b35f2de6066
IgorOks <winbka.lem@gmail.com>
2022-12-23 14:31:39 +0200

    ls_17 | Created products  module

---
+++ src/app/administration/products/products.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IData } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent implements OnInit {
+  public products: IData[];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.products = this.productsService.data;
+  }
+}
