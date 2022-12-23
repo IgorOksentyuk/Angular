3883c875ac6c434fae31b72b3bca29d36534b282
IgorOks <winbka.lem@gmail.com>
2022-12-23 12:20:10 +0200

    ls_17 | Removed not found component

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -7,6 +7,11 @@ import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
     path: 'products',
     pathMatch: 'full',
     component: ProductsComponent,
