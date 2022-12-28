858311e211cd6bae3e6f53dc9b5461227402b2f0
IgorOks <winbka.lem@gmail.com>
2022-12-28 16:15:23 +0200

    ls_18 | Added loading logic

--- src/app/administration/products/products.component.ts
+++ src/app/administration/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IData } from 'src/app/models/product.model';
+import { LoadingService } from 'src/app/services/loading.service';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
@@ -9,12 +10,20 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class ProductsComponent implements OnInit {
   public products: IData[];
+  loading$ = this.loadingSvc._loading$;
 
-  constructor(private productsService: ProductsService) {}
+  constructor(
+    private productsService: ProductsService,
+    private loadingSvc: LoadingService
+  ) {}
 
   ngOnInit(): void {
+    this.loadingSvc.showLoader();
+
     this.productsService.data.subscribe((res) => {
       this.products = res;
+
+      this.loadingSvc.hideLoader();
     });
   }
 }
