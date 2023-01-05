05596eaaab1a873e950bc608f96a28a0c9a5a533
IgorOks <winbka.lem@gmail.com>
2023-01-05 12:35:31 +0200

    ls_19 | Imported Ng2SearchPipeModule and FormsModule

--- src/app/administration/products/products.module.ts
+++ src/app/administration/products/products.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
 
 import { NavigationModule } from '../navigation/navigation.module';
 import { ProductsRoutingModule } from './products-routing.module';
 
 import { ProductsComponent } from './products.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, FilterComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule,
     NavigationModule,
     MatProgressSpinnerModule,
+    Ng2SearchPipeModule,
+    FormsModule,
   ],
-  exports: [ProductsComponent, MatProgressSpinnerModule],
+  exports: [ProductsComponent, MatProgressSpinnerModule, FilterComponent],
 })
 export class ProductsModule {}
