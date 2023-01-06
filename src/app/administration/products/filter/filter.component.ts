28eda8b3f574a31c3433c50f2cb4bc8eed7c07db
IgorOks <winbka.lem@gmail.com>
2023-01-06 15:35:12 +0200

    ls_19 | Added filter logic

--- src/app/administration/products/filter/filter.component.ts
+++ src/app/administration/products/filter/filter.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 import { ProductsService } from 'src/app/services/products.service';
 import { FilterService } from 'src/app/services/filter.service';
-import { IData } from '../../../models/product.model';
+import {
+  FilterConfiguration,
+  TypeOfFilterPrice,
+} from '../../products/filter/models/filter.model';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-filter',
@@ -11,20 +16,34 @@ import { IData } from '../../../models/product.model';
 })
 export class FilterComponent implements OnInit {
   visibleTool: boolean = false;
+  priceMore = TypeOfFilterPrice.More;
+  priceLess = TypeOfFilterPrice.Less;
+
+  @Output()
+  filterChangeEvent = new EventEmitter<FilterConfiguration>();
 
   constructor(
     private productsService: ProductsService,
     private filterService: FilterService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.filterService.configuration$
+      .pipe(debounceTime(1000))
+      .subscribe((filterConfig) => {
+        this.filterChangeEvent.emit(filterConfig);
+      });
+  }
 
-  // @HostListener('click', ['$event'])
-  // onClick(event: Event) {
-  //   // console.log(event.target);
-  // }
+  price(event: any): void {
+    this.filterService.setPrice(event.target.value);
+  }
 
-  toggle() {
-    this.visibleTool = !this.visibleTool;
+  priceType(event: any): void {
+    this.filterService.setTypePrice(event.target.value);
+  }
+
+  search(event: any): void {
+    this.filterService.setSearchValue(event.target.value);
   }
 }
