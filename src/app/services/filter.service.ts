05e6fd556e59dce9517872cc1ac9a6dff9f8f98f
IgorOks <winbka.lem@gmail.com>
2023-01-06 15:28:49 +0200

    ls_19 | Added filter, sort and search logic

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import {
+  FilterConfiguration,
+  TypeOfFilterPrice,
+} from '../administration/products/filter/models/filter.model';
+
+const DEFAULT_CONFIGARATION: FilterConfiguration = {
+  price: 0,
+  priceType: TypeOfFilterPrice.More,
+  search: '',
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class FilterService {
+  private _configuration$: BehaviorSubject<FilterConfiguration> =
+    new BehaviorSubject(DEFAULT_CONFIGARATION);
+
+  public configuration$: Observable<FilterConfiguration> =
+    this._configuration$.asObservable();
+
+  get currentConfiguration(): FilterConfiguration {
+    return this._configuration$.getValue();
+  }
+
   constructor() {}
+
+  setPrice(price: number): void {
+    this._configuration$.next({
+      ...this.currentConfiguration,
+      price,
+    });
+  }
+
+  setTypePrice(priceType: TypeOfFilterPrice): void {
+    this._configuration$.next({
+      ...this.currentConfiguration,
+      priceType,
+    });
+  }
+
+  setSearchValue(search: string): void {
+    this._configuration$.next({
+      ...this.currentConfiguration,
+      search,
+    });
+  }
 }
