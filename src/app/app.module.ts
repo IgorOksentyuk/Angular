6927f9f65fbe09c7437ed90c23d8fe5b7acab21f
IgorOks <winbka.lem@gmail.com>
2023-01-09 20:32:21 +0200

    ls_22 | Added imports for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ProductsModule } from './products/products.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AdministrationModule } from './administration/administration.module';
+import { LoginModule } from './login/login.module';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { authInterceptorProviders } from '../app/login/services/auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent, ProductsComponent, NotFoundComponent],
@@ -20,8 +24,14 @@ import { HttpClientModule } from '@angular/common/http';
     AdministrationModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    LoginModule,
+    JwtModule,
   ],
-  providers: [],
+  providers: [
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService,
+    authInterceptorProviders,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
