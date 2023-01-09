c7f57678140448478687f5d36cdfd9837638b6c5
IgorOks <winbka.lem@gmail.com>
2023-01-09 12:04:11 +0200

    ls_21 | Reworked component with backend logic

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IData } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
 import { ProductsService } from 'src/app/services/products.service';
@@ -11,19 +11,26 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IData | undefined;
-  products: IData[];
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private svc: ProductsService,
-    private cartService: CartService
+    private cartService: CartService,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    const productId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
-    this.svc.data.subscribe((res) => {
-      this.products = res;
-      this.product = this.products.find((el) => el.id == productId);
+    const productId = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!productId) {
+      this.router.navigateByUrl(`/404`);
+    }
+
+    this.svc.getById(productId as string).subscribe((res) => {
+      if (res === null) {
+        this.router.navigateByUrl(`/404`);
+        return;
+      }
+      this.product = res;
     });
   }
 
