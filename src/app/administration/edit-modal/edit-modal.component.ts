9e1def595cc1acab7f222281e15aca1467253ea6
IgorOks <winbka.lem@gmail.com>
2023-01-09 11:54:42 +0200

    ls_20 | Created modal dialog for edit products

---
+++ src/app/administration/edit-modal/edit-modal.component.ts
@@ -0,0 +1,85 @@
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  Optional,
+  Inject,
+} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { ModalService } from 'src/app/services/modal.service';
+import { ProductsService } from 'src/app/services/products.service';
+import { ProductConfiguration } from '../modal/models/ProductsConfiguration.model';
+import { IData } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-edit-modal',
+  templateUrl: './edit-modal.component.html',
+  styleUrls: ['../modal/modal.component.scss'],
+})
+export class EditModalComponent {
+  productId: string;
+  productName: string = '';
+  productPrice: number = 0;
+  productDescription: string = '';
+
+  @Output()
+  productEditvent = new EventEmitter<ProductConfiguration>();
+
+  @ViewChild('productname') inputName: ElementRef<HTMLInputElement>;
+  @ViewChild('productprice') inputPrice: ElementRef<HTMLInputElement>;
+  @ViewChild('productdescription')
+  inputDescription: ElementRef<HTMLInputElement>;
+
+  constructor(
+    private dialogRef: MatDialogRef<EditModalComponent>,
+    private modalService: ModalService,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.productName = data.productName;
+    this.productPrice = data.productPrice;
+    this.productId = data.productId;
+    this.productDescription = data.productDescription;
+  }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.inputName.nativeElement.value = this.productName;
+    this.modalService.setName(this.productName);
+
+    this.inputPrice.nativeElement.value = String(this.productPrice);
+    this.modalService.setPrice(this.productPrice);
+
+    this.inputDescription.nativeElement.value = String(this.productDescription);
+    this.modalService.setDescription(this.productDescription);
+
+    this.modalService.setId(this.productId);
+  }
+
+  setName(event: any): void {
+    this.modalService.setName(event.target.value);
+  }
+
+  setPrice(event: any): void {
+    this.modalService.setPrice(event.target.value);
+  }
+
+  setDescription(event: any): void {
+    this.modalService.setDescription(event.target.value);
+  }
+
+  close() {
+    this.dialogRef.close(false);
+  }
+
+  ok() {
+    this.modalService.configuration$.subscribe((productConfig) => {
+      this.productEditvent.emit(productConfig);
+
+      this.dialogRef.close(productConfig);
+    });
+  }
+}
