92569e482e1bfee50ba7eb528969a95179e68a0d
Igor Oksentyuk <winbka.lem@gmail.com>
2023-03-24 13:30:22 +0200

    Created users service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '../models/user.model';
@@ -9,10 +9,26 @@ import { User } from '../models/user.model';
 })
 export class UsersService {
   baseUrl: string = 'https://hys-fe-course-api.vercel.app/users';
+  private _createUserSubj$ = new Subject<User>();
 
   constructor(private http: HttpClient) {}
 
+  newCreateEvent(user: User) {
+    this._createUserSubj$.next(user);
+  }
+
+  get createEvents$() {
+    return this._createUserSubj$.asObservable();
+  }
+
   getAll(): Observable<User[]> {
     return this.http.get(this.baseUrl) as Observable<User[]>;
+  }
+
+  create(user: User): Observable<User> {
+    return this.http.post(this.baseUrl, {
+      username: user.username,
+      pasword: user.password,
+    }) as Observable<User>;
   }
 }
